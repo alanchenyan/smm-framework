611d095cb92404685db76298a35bf43a847c5d1c
MacBook <chenyan900520@126.com>
2019-10-08 17:04:53 +0800

    1.0.4

--- src/main/java/com/smm/framework/config/GloablSwaggerConfig.java
+++ src/main/java/com/smm/framework/config/GloablSwaggerConfig.java
@@ -1,21 +1,26 @@
 package com.smm.framework.config;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @Author -Huang
  * @create 2019/9/3 9:02
  */
 @Configuration
-public abstract class GloablSwaggerConfig extends WebMvcConfigurationSupport {
+@EnableSwagger2
+public abstract class GloablSwaggerConfig implements WebMvcConfigurer {
 
     /**
      * Swagger文档标题
@@ -47,12 +52,17 @@ public abstract class GloablSwaggerConfig extends WebMvcConfigurationSupport {
                 .build();
     }
 
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
 }
