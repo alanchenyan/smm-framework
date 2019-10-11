cda8a06f756ca8a719dab4f19df9d5eb2e0e9db6
MacBook <chenyan900520@126.com>
2019-10-11 20:46:20 +0800

    全局包装返回值

---
+++ src/main/java/com/smm/framework/config/GlobalReturnConfig.java
@@ -0,0 +1,51 @@
+package com.smm.framework.config;
+
+import com.smm.framework.exception.ServiceException;
+import com.smm.framework.response.ResponseResult;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author Alan Chen
+ * @description
+ * @date 2019-10-11
+ */
+@EnableWebMvc
+@Configuration
+public class GlobalReturnConfig {
+
+    @RestControllerAdvice
+    static class ResultResponseAdvice implements ResponseBodyAdvice<Object> {
+        @Override
+        public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+            return true;
+        }
+
+        @Override
+        public Object beforeBodyWrite(Object returnObj, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+
+            if(returnObj==null){ // 返回值为void
+                return ResponseResult.success();
+            }
+
+            //全局异常会拦截统一封装成ResponseResult对象，因此不需要再包装了
+            if(returnObj instanceof ResponseResult){
+                return returnObj;
+            }
+
+            if(returnObj instanceof String){
+                throw new ServiceException("Controller层暂时不支持返回String类型参数。如需返回String参数，请用ResponseResult.success(obj)方式返回");
+            }
+
+            return  ResponseResult.success(returnObj);
+
+        }
+    }
+}
