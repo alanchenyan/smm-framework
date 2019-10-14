07636f63bce98ef9963ff2ff81c9681f7efe9fcc
MacBook <chenyan900520@126.com>
2019-10-14 12:27:11 +0800

    basepackage

--- src/main/java/com/smm/framework/config/GlobalReturnConfig.java
+++ src/main/java/com/smm/framework/config/GlobalReturnConfig.java
@@ -17,38 +17,40 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
  * @description Controller返回参数全局包装成ResponseResult对象
  * @date 2019-10-11
  */
+/**
+ * @author Alan Chen
+ * @description Controller返回参数全局包装成ResponseResult对象
+ *              使用是一般需要指定basePackages，@RestControllerAdvice(basePackages = {"com.netx.web.controller"})
+ *              只拦截controller包下的类；否则swagger也会拦截影响swagger正常使用
+ * @date 2019-10-11
+ */
 @EnableWebMvc
 @Configuration
-public class GlobalReturnConfig {
+@RestControllerAdvice
+public class GlobalReturnConfig  implements ResponseBodyAdvice<Object> {
 
-    /**
-     *  basePackages : 只拦截controller包下的类；否则swagger也会拦截影响swagger正常使用
-     */
-    @RestControllerAdvice(basePackages = "*.controller")
-    static class ResultResponseAdvice implements ResponseBodyAdvice<Object> {
-        @Override
-        public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
-            return true;
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object returnObj, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+
+        if(returnObj==null){ // 返回值为void
+            return ResponseResult.success();
         }
 
-        @Override
-        public Object beforeBodyWrite(Object returnObj, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
-
-            if(returnObj==null){ // 返回值为void
-                return ResponseResult.success();
-            }
-
-            //全局异常会拦截统一封装成ResponseResult对象，因此不需要再包装了
-            if(returnObj instanceof ResponseResult){
-                return returnObj;
-            }
-
-            if(returnObj instanceof String){
-                throw new ServiceException("Controller层暂时不支持返回String类型参数。如需返回String参数，请用ResponseResult.success(obj)方式返回");
-            }
-
-            return  ResponseResult.success(returnObj);
-
+        //全局异常会拦截统一封装成ResponseResult对象，因此不需要再包装了
+        if(returnObj instanceof ResponseResult){
+            return returnObj;
         }
+
+        if(returnObj instanceof String){
+            throw new ServiceException("Controller层暂时不支持返回String类型参数。如需返回String参数，请用ResponseResult.success(obj)方式返回");
+        }
+
+        return  ResponseResult.success(returnObj);
+
     }
 }
