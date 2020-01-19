b8d34f682b157d80e1df4549b04dce46204e6e00
MacBook <chenyan900520@126.com>
2020-01-19 18:19:06 +0800

    操作人

--- src/main/java/com/smm/framework/config/mybatis/GlobalMetaObjectHandler.java
+++ src/main/java/com/smm/framework/config/mybatis/GlobalMetaObjectHandler.java
@@ -4,7 +4,11 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 /**
@@ -16,6 +20,8 @@ public class GlobalMetaObjectHandler implements MetaObjectHandler {
 
     private Logger logger = LoggerFactory.getLogger(GlobalMetaObjectHandler.class);
 
+    String createUserIdFieldName = "createUserId";
+    String updateUserIdFieldName = "updateUserId";
     String createTimeFieldName = "createTime";
     String updateTimeFieldName = "updateTime";
     String deletedFieldName = "deleted";
@@ -23,9 +29,14 @@ public class GlobalMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         try {
+            Object createUserId = getFieldValByName(createUserIdFieldName, metaObject);
             Object createTime = getFieldValByName(createTimeFieldName, metaObject);
             Object updateTime = getFieldValByName(updateTimeFieldName, metaObject);
             Object delTag = getFieldValByName(deletedFieldName, metaObject);
+
+            if(null ==createUserId){
+                setFieldValByName(createUserIdFieldName, getUserId(), metaObject);
+            }
 
             Date date = new Date();
             if (null == createTime) {
@@ -45,8 +56,21 @@ public class GlobalMetaObjectHandler implements MetaObjectHandler {
     public void updateFill(MetaObject metaObject) {
         try {
             setFieldValByName(updateTimeFieldName, new Date(), metaObject);
+            setFieldValByName(updateUserIdFieldName, getUserId(), metaObject);
         } catch (Exception e) {
             logger.warn(e.getMessage(), e);
         }
+    }
+
+    private String getUserId(){
+        String userId = null;
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if(requestAttributes !=null){
+            HttpServletRequest request = ((ServletRequestAttributes)requestAttributes).getRequest();
+            if(request!=null){
+                userId = request.getHeader("userId");
+            }
+        }
+        return userId;
     }
 }
