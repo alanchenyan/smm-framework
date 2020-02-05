188741db35f39cdced72e8b9a309230810ac21f1
MacBook <chenyan900520@126.com>
2020-02-05 11:10:25 +0800

    ac

---
+++ src/main/java/com/smm/framework/authority/GlobalUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.smm.framework.authority;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.context.request.RequestContextListener;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alan Chen
+ * @description 校验账号
+ * @date 2020-02-05
+ */
+public class GlobalUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    HttpServletRequest request;
+
+    @Bean
+    public RequestContextListener requestContextListener(){
+        return new RequestContextListener();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
