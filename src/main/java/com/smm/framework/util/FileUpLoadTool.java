c8b265a42237769ba6763ef64f8086d456d20022
MacBook <chenyan900520@126.com>
2020-04-21 14:29:03 +0800

    ac

--- src/main/java/com/smm/framework/util/FileUpLoadTool.java
+++ src/main/java/com/smm/framework/util/FileUpLoadTool.java
@@ -17,7 +17,7 @@ import java.io.IOException;
  */
 public class FileUpLoadTool {
 
-    private static final long DEFAULT_MAX_SIZE_UNIT_KB = 1024 * 5;
+    private static final long DEFAULT_MAX_SIZE_UNIT_KB = 1024 * 10;
 
     private static final String DEFALUT_UPLOAD_FILE_DIRECTORY = "/uploadfiles/";
 
