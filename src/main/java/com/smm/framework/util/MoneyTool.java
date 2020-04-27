7f900567e89b9a743973e7d22c5f4e324f59c6a2
MacBook <chenyan900520@126.com>
2020-04-27 14:48:57 +0800

    ac

---
+++ src/main/java/com/smm/framework/util/MoneyTool.java
@@ -0,0 +1,46 @@
+package com.smm.framework.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+/**
+ * @author Alan Chen
+ * @description 钱单位转换工具
+ * @date 2020-04-03
+ */
+public class MoneyTool {
+
+    final static DecimalFormat df = new DecimalFormat("#0.00");
+
+    private MoneyTool(){
+
+    }
+
+    public static Long yuan2Fen(String yuan){
+        if(StringUtils.isNotBlank(yuan)){
+            return new BigDecimal(yuan).multiply(new BigDecimal(100)).longValue();
+        }else{
+            return 0L;
+        }
+    }
+
+    public static String fen2Yuan(String fen){
+        if(StringUtils.isNotBlank(fen)){
+            BigDecimal yuan = BigDecimal.valueOf(Long.valueOf(fen)).divide(new BigDecimal(100));
+            return df.format(yuan);
+        }else{
+            return "0.00";
+        }
+    }
+
+    public static String fen2Yuan(Long fen){
+        if(fen!=null){
+            BigDecimal yuan = BigDecimal.valueOf(Long.valueOf(fen)).divide(new BigDecimal(100));
+            return df.format(yuan);
+        }else{
+            return "0.00";
+        }
+    }
+}
