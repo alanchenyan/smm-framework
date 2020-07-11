160cb96fff4abfba917a337d35c9b3361a9bac9d
MacBook <chenyan900520@126.com>
2020-07-11 10:54:35 +0800

    a

--- src/main/java/com/smm/framework/util/NumberTool.java
+++ src/main/java/com/smm/framework/util/NumberTool.java
@@ -72,6 +72,18 @@ public class NumberTool {
     }
 
     /**
+     * 提供精确的乘法运算
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double mul(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2).doubleValue();
+    }
+
+    /**
      * 提供（相对）精确的除法运算，当发生除不尽的情况时，精确到
      * 小数点以后10位，以后的数字四舍五入。
      * @param v1 被除数
