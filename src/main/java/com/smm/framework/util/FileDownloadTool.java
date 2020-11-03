353ce60d29bab8d0d167641aa650f1e5024095c5
AlanChen <chenyan900520@126.com>
2020-11-03 17:13:51 +0800

    ac

--- src/main/java/com/smm/framework/util/FileDownloadTool.java
+++ src/main/java/com/smm/framework/util/FileDownloadTool.java
@@ -1,9 +1,12 @@
 package com.smm.framework.util;
 
 import com.smm.framework.exception.ServiceException;
+import sun.misc.BASE64Encoder;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * @author Alan Chen
@@ -13,6 +16,41 @@ import java.io.*;
 public class FileDownloadTool {
 
     private FileDownloadTool(){}
+
+    public static String downloadPicture(String picUrl) {
+        String pictureName = FileUpLoadTool.getRandomImageName()+".jpg";
+        String defaultPath = FileUpLoadTool.getDefalutUploadFilesDirectory()+"/"+pictureName;
+        downloadPicture(picUrl,defaultPath);
+        return FileUpLoadTool.FILE_REDIRECT_NAME+"/"+pictureName;
+    }
+
+    public static void downloadPicture(String picUrl,String path) {
+        URL url = null;
+        try {
+            url = new URL(picUrl);
+            DataInputStream dataInputStream = new DataInputStream(url.openStream());
+
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = dataInputStream.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+            BASE64Encoder encoder = new BASE64Encoder();
+            String encode = encoder.encode(buffer);//返回Base64编码过的字节数组字符串
+            System.out.println(encode);
+            fileOutputStream.write(output.toByteArray());
+            dataInputStream.close();
+            fileOutputStream.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void downLoadFile(HttpServletResponse response, String fileUrl) {
         String filePath = fileUrl;
